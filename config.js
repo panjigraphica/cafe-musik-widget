10482e35454dcddb8e54b99826789857a8a9a39a
panjigraphica <147980618+panjigraphica@users.noreply.github.com>
2026-06-16 21:35:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   CLIENT_ID: "e4f09ace85354ef196201a7fccda6798",
 
   REDIRECT_URI:
-"https://panjigraphica.github.io/cafe-musik-widget/callback.html",
+  "https://panjigraphica.github.io/cafe-musik-widget/callback.html",
 
   SCOPES: [
     "user-read-currently-playing",
