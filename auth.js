a82aee19255b9c5d5fc762a9c12fea9d35100647
panjigraphica <147980618+panjigraphica@users.noreply.github.com>
2026-06-18 05:33:10 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,8 @@
 async function loginSpotify() {
 
-  const spotifyUrl =
-    "https://accounts.spotify.com/authorize" +
-    "?client_id=e4f09ace85354ef196201a7fccda6798" +
-    "&response_type=code" +
-    "&redirect_uri=https%3A%2F%2Fpanjigraphica.github.io%2Fcafe-musik-widget%2Fcallback.html" +
-    "&scope=user-read-currently-playing%20user-read-playback-state";
+  alert("TOMBOL DIKLIK");
 
-  window.open(spotifyUrl, "_self");
+  window.location.href =
+    "https://accounts.spotify.com";
 
 }
