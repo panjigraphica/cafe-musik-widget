fe351eadfa1a5f346b415ab15759c5b0ab0546eb
panjigraphica <147980618+panjigraphica@users.noreply.github.com>
2026-06-18 05:01:29 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,14 @@
 async function loginSpotify() {
-  alert("TEST");
 
-  window.location.href =
-    "https://www.google.com";
+  const spotifyUrl =
+    "https://accounts.spotify.com/authorize" +
+    "?client_id=e4f09ace85354ef196201a7fccda6798" +
+    "&response_type=code" +
+    "&redirect_uri=" +
+    encodeURIComponent(
+      "https://panjigraphica.github.io/cafe-musik-widget/callback.html"
+    ) +
+    "&scope=user-read-currently-playing%20user-read-playback-state";
+
+  window.location.href = spotifyUrl;
 }
