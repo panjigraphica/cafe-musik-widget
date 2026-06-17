860c6d63cb31780598e2f999f6281a33a9091c2a
panjigraphica <147980618+panjigraphica@users.noreply.github.com>
2026-06-18 05:08:51 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,3 @@
 async function loginSpotify() {
-
-  const spotifyUrl =
-    "https://accounts.spotify.com/authorize" +
-    "?client_id=e4f09ace85354ef196201a7fccda6798" +
-    "&response_type=code" +
-    "&redirect_uri=" +
-    encodeURIComponent(
-      "https://panjigraphica.github.io/cafe-musik-widget/callback.html"
-    ) +
-    "&scope=user-read-currently-playing%20user-read-playback-state";
-
-  window.location.href = spotifyUrl;
-
+  alert("LOGIN BERHASIL DIKLIK");
 }
